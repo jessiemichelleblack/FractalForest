94ad1add96ed38c0037e1cab994dd5e78fdb7331
Jessie Albarian <albarian.jess@gmail.com>
2017-12-09 14:09:36 -0700

    All of the code for the audio is working. Currently adding audio files to mushrooms manually. Music stuff was combined with the laser stuff

--- New Unity Project/Assets/FractalForest/Scripts/MajorLazers.cs
+++ New Unity Project/Assets/FractalForest/Scripts/MajorLazers.cs
@@ -94,9 +94,15 @@ public class MajorLazers : MonoBehaviour
                 reticle.SetActive(true);
                 teleportReticleTransform.position = hitPoint + teleportReticleOffset;
 
+                // Git the plant object that the laser is hitting
                 GameObject hitPlant = hit.transform.gameObject;
-                Debug.Log(hitPlant.transform.name);
-                hitPlant.GetComponent<AudioSource>().Play();
+                AudioSource hitPlantAudio = hitPlant.GetComponent<AudioSource>();
+
+                // Keep the audio from sporadically restarting
+                if (!hitPlantAudio.isPlaying)
+                {
+                    hitPlantAudio.Play();
+                }
                 
             }
         }
